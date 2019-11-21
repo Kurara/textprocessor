2acd704e07827b20a0a4faf7a5a233e5aa70e127
usuario <Kurarab2p@gmail.com>
2019-11-21 21:26:06 +0100

    exercise during class

--- main.py
+++ main.py
@@ -1,10 +1,67 @@
 import csv
+import json
+import yaml
+import xml.etree.ElementTree as ET
 
 
 class TextProcessor:
 
     def read_csv(self, file):
-        with open(file, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
+        self.list_rows = []
+        with open(file, 'r') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=',')
             for row in reader:
-                print(row['macchina'], row['modello'])
+                self.list_rows.append(row)
+
+
+    def write_csv(self, file):
+        with open(file, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile)
+            for row in self.list_rows:
+                writer.writerow(row)
+            # writer.writerows(self.list_rows)
+            
+    def read_json(self, file):
+        # to_load = "{'machinaria': 'si'}"
+        with open(file, 'r') as jsonfile:
+            dict_values = json.loads(jsonfile.read())
+        print(dict_values)
+
+    def write_json(self, file, py_dict):
+        json_str = json.dumps(py_dict, indent=3)
+        with open(file, 'w') as jsonfile:
+            jsonfile.write(json_str)
+
+    def read_yaml(self, file):
+        # to_load = "{'machinaria': 'si'}"
+        with open(file, 'r') as jsonfile:
+            dict_values = yaml.load(jsonfile.read())
+        print(dict_values)
+
+    def write_yaml(self, file, py_dict):
+        json_str = yaml.dump(py_dict, indent=3)
+        with open(file, 'w') as jsonfile:
+            jsonfile.write(json_str)
+
+    def read_xml(self, file):
+        tree = ET.parse(file)
+        root = tree.getroot()
+        machine = root.getchildren()
+
+    def modify_xml(self, file):
+        tree = ET.parse(file)
+        root = tree.getroot()
+        machine = root.findall("macchina")
+        for machina in machine:
+            prezzi = machina.find("prezzi").attrib
+            prezzo_iniziale = prezzi.get('compra')
+            benefici = machina.find("benefici")
+            try:
+                prezzo_iniziale = int(prezzo_iniziale)
+                benefici_int = int(benefici.text)
+            except Exception:
+                prezzo_iniziale = 0
+            prezzo_finale = prezzo_iniziale + benefici_int
+            machina.find("prezzi").set('vendita', str(prezzo_finale))
+        tree.write(file)
+        
--- test.py
+++ test.py
@@ -5,9 +5,65 @@ from main import TextProcessor
 class TestProcessor(unittest.TestCase):
 
     def setUp(self):
-        self.BASE_PATH = ""
+        import os
+        self.BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
-    def test_csv_read(self):
-        filepath = self.BASE_PATH + "mockups/concessionario.csv"
+    def un_test_csv_read(self):
+        filepath = self.BASE_PATH + "/mockups/concessionario.csv"
         processor = TextProcessor()
         processor.read_csv(filepath)
+
+    def un_test_csv_writter(self):
+        filepath = self.BASE_PATH + "/mockups/concessionario.csv"
+        processor = TextProcessor()
+        processor.read_csv(filepath)
+
+        filepath2 = self.BASE_PATH + "/mockups/concessionario.csv"
+        processor.write_csv(filepath2)
+
+    def test_json_reader(self):
+        filepath = self.BASE_PATH + "/mockups/concessionario.json"
+        processor = TextProcessor()
+        processor.read_json(filepath)
+
+    def test_json_writer(self):
+        filepath = self.BASE_PATH + "/mockups/nuovo_json.json"
+        processor = TextProcessor()
+        to_parse = {
+            "nome": "Silvia",
+            "cognome": "Rossi",
+            "admin": False,
+            "mascote": 5,
+            "figli": {
+                "femmine": ["Clara", "Francesca"],
+                "maschi": []
+            } 
+        }
+        processor.write_json(filepath, to_parse)
+
+    def test_yalm_reader(self):
+        filepath = self.BASE_PATH + "/mockups/concessionario.yml"
+        processor = TextProcessor()
+        processor.read_yaml(filepath)
+
+    def test_yalm_writer(self):
+        filepath = self.BASE_PATH + "/mockups/nuovo_yaml.yml"
+        processor = TextProcessor()
+        to_parse = {
+            "nome": "Silvia",
+            "cognome": "Rossi",
+            "admin": False,
+            "mascote": 5,
+            "figli": {
+                "femmine": ["Clara", "Francesca"],
+                "maschi": []
+            } 
+        }
+        processor.write_yaml(filepath, to_parse)
+
+    def test_read_xml(self):
+        filepath = self.BASE_PATH + "/mockups/concessionario.xml"
+        processor = TextProcessor()
+        processor.read_xml(filepath)
+
+        processor.modify_xml(filepath)
